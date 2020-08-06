93fe67a0288e4903907193c2203cb57b0be17cca
shaileshnit <thakur.sailesh.thakur@gmail.com>
2020-08-07 03:31:42 +0530

    java programs

---
+++ 2020/2.operators.java
@@ -0,0 +1,10 @@
+class Operators {
+    public static void main(String[] args) {
+        String s = new String("ashok");
+        System.out.println(s == null); // output : false
+        String r = null;
+        System.out.println(r == null); // true
+        System.out.println(null == null);
+        // Test obj = new Test(); 
+    }
+};
